b8a7462e86a31a32a03b3ad91c1b4f388d555c4a
Tav <silvermill123@gmail.com>
2021-06-22 18:05:35 +0300

    added r2dbc and postgresql dependencies, configured r2dbc connection and logging

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,12 +17,15 @@ repositories {
 
 dependencies {
 	implementation("com.expediagroup:graphql-kotlin-spring-server:5.0.0-alpha.0")
+	implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+	runtimeOnly("io.r2dbc:r2dbc-postgresql")
+	runtimeOnly("org.postgresql:postgresql")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("io.projectreactor:reactor-test")
