3cdf1b9a54d3c77d9fea5096616f9825b29edb86
Tav <silvermill123@gmail.com>
2021-07-01 14:40:50 +0300

    Removed unnecessary config

--- src/main/kotlin/com/tav/reactive/graphql/fetcher/CustomFunctionDataFetcher.kt
+++ src/main/kotlin/com/tav/reactive/graphql/fetcher/CustomFunctionDataFetcher.kt
@@ -12,7 +12,6 @@ import kotlin.reflect.KFunction
 class CustomFunctionDataFetcher(target: Any?, fn: KFunction<*>, objectMapper: ObjectMapper) : FunctionDataFetcher(target, fn, objectMapper) {
   override fun get(environment: DataFetchingEnvironment): Any? = when (val result = super.get(environment)) {
     is Mono<*> -> result.toFuture()
-    is Flux<*> -> result.collectList().toFuture()
     else -> result
   }
 }
